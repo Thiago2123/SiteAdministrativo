1185c21149ce1f44226bba23ab484fde139639b7
Thiago Meneghetti Rossi <thiago.meneghetti@clubedodev.com.br>
2024-02-15 17:26:37 -0300

    SiteAdimistrativoCreated

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+document.querySelector('#menu-btn').addEventListener('click' , function(){
+    // alert('click');
+    document.querySelector('#menu-site').classList.toggle("active");
+    document.querySelector('#menu-icon').classList.toggle("active");
+});
